eaa7e910ad44fc0b7717bb151cd9b33611050240
oAtomH <alessiohoxhaj@gmail.com>
2024-10-31 20:23:14 +0100

    tasduh9ashuoduhas

--- kubejs/startup_scripts/container.js
+++ kubejs/startup_scripts/container.js
@@ -1,8 +1,3 @@
-StartupEvents.registry('block', event => {
-    event.create('primitive_chest').soundType('wood').blockEntity(entityInfo => {
-        entityInfo.inventory(9,1)
-        entityInfo.rightClickOpensInventory()
-    })
-})
 
-//this is a test for github repository asujoasuoduaosa test 2307 23761 auhasduyhgygu9adsg9y7as
+
+//this is a test fasdnuioasuodhy792y79adasdasaosa test 2307 23761 auhasduyhgygu9adsg9y7as
